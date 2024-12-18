775b76017b66b5ea5fd4b8e5f72adc0f7c48c58c
Mouad-Ag <mouadag10@gmail.com>
2024-12-18 21:37:24 +0100

    refactor cycle 2

--- src/main/java/com/controle/tdd/FizzBuzz.java
+++ src/main/java/com/controle/tdd/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.controle.tdd;
 
 public class FizzBuzz {
     public static String de(int i) {
-        if (i == 3) return "Fizz";
+        if (i % 3 == 0) return "Fizz";
         return String.valueOf(i);
     }
 }
