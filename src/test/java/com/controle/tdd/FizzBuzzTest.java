271bd6e514ca00eeab978b7886b07fa275ba69d7
Mouad-Ag <mouadag10@gmail.com>
2024-12-18 22:00:21 +0100

    Test paramétré : FizzBuzz.de(1 à 20)

--- src/test/java/com/controle/tdd/FizzBuzzTest.java
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 package com.controle.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
@@ -24,4 +26,15 @@ public class FizzBuzzTest {
     public void testFizzBuzzOtherNumbers() {
         assertEquals("7", FizzBuzz.de(7));
     }
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1", "2, 2", "3, Fizz", "4, 4", "5, Buzz",
+            "6, Fizz", "7, 7", "8, 8", "9, Fizz", "10, Buzz",
+            "11, 11", "12, Fizz", "13, 13", "14, 14", "15, FizzBuzz",
+            "16, 16", "17, 17", "18, Fizz", "19, 19", "20, Buzz"
+    })
+    public void testFizzBuzzParametrized(int input, String expected) {
+        assertEquals(expected, FizzBuzz.de(input));
+    }
+
 }
